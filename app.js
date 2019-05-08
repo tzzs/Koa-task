b2c5bc8d573c860e10854c9a7e64f940844d746d
tzz <1789803837@qq.com>
2019-05-08 19:26:52 +0800

    完成koa post数据接收 修改缩进为2个空格

--- app.js
+++ app.js
@@ -2,8 +2,10 @@
 
 const Koa = require('koa');
 const Router = require('koa-router');
-const render = require('./tools/render');
 const bodyParser = require('koa-bodyparser')
+const logger = require('koa-logger')
+
+const render = require('./tools/render');
 
 const app = module.exports = new Koa();
 const router = new Router();
@@ -11,35 +13,41 @@ const router = new Router();
 app.use(bodyParser());
 
 app.use(async function (ctx, next) {
-    await next();
-    console.log(ctx.status);
-    if (ctx.status === 404) {
-        ctx.set('Content-Type', 'text/html;charset="utf-8"');
-        const data = await render('404.html')
-        ctx.body = data;
-    }
-    // ctx.body = 'Hello World';
+  await next();
+  console.log(ctx.status);
+  if (ctx.status === 404) {
+    ctx.set('Content-Type', 'text/html;charset="utf-8"');
+    const data = await render('404.html')
+    ctx.body = data;
+  }
+  // ctx.body = 'Hello World';
 });
 
 router.get('/', async (ctx) => {
-    ctx.set('Content-Type', 'text/html;charset="utf-8"');
-    const data = await render('index.html');
-    ctx.body = data;
+  ctx.set('Content-Type', 'text/html;charset="utf-8"');
+  const data = await render('index.html');
+  ctx.body = data;
+})
+
+router.post('/', async (ctx) => {
+  ctx.set('Content-Type', 'application/json;charset="utf-8"');
+  const query = ctx.request.query;
+  ctx.body = query;
 })
 
 router.get('/index', async (ctx) => {
-    ctx.set('Content-Type', 'text/html;charset="utf-8"');
-    const data = await render('index.html');
-    ctx.body = data;
+  ctx.set('Content-Type', 'text/html;charset="utf-8"');
+  const data = await render('index.html');
+  ctx.body = data;
 })
 
 router.get('/hello', async (ctx) => {
-    ctx.set('Content-Type', 'text/html;charset="utf-8"');
-    const data = await render('hello-world.html');
-    ctx.body = data;
+  ctx.set('Content-Type', 'text/html;charset="utf-8"');
+  const data = await render('hello-world.html');
+  ctx.body = data;
 })
 
 app.use(router.routes()).use(router.allowedMethods());
 app.listen(8080, () => {
-    console.log('Sever satrt at 127.0.0.1:8080');
+  console.log('Sever satrt at 127.0.0.1:8080');
 });
--- tools/render.js
+++ tools/render.js
@@ -3,15 +3,15 @@
 const path = require('path');
 const fs = require('fs');
 async function render(page) {
-    const viewUrl = path.join(__dirname, '..', `/view/${page}`);
-    return new Promise((reslove, reject) => {
-        fs.readFile(viewUrl, (err, data) => {
-            if (err) {
-                return reject(`<h1>inner error! ${err}</h1>`);
-            }
-            return reslove(data);
-        })
+  const viewUrl = path.join(__dirname, '..', `/view/${page}`);
+  return new Promise((reslove, reject) => {
+    fs.readFile(viewUrl, (err, data) => {
+      if (err) {
+        return reject(`<h1>inner error! ${err}</h1>`);
+      }
+      return reslove(data);
     })
+  })
 }
 
 module.exports = render
