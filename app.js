140a9cd8a18ec9deac5fc87c29062207c23e649f
tzz <1789803837@qq.com>
2019-05-10 19:09:17 +0800

    使用参数携带token 解决携带token进行页面跳转的问题

--- app.js
+++ app.js
@@ -13,18 +13,18 @@ const auth = require('./services/auth')
 const app = module.exports = new Koa();
 
 
-// 错误处理
-app.use((ctx, next) => {
-  return next().catch((err) => {
-    if (err.status === 401) {
-      ctx.status = 401;
-      ctx.body = 'Protected resource, use Authorization header to get access\n';
-    } else {
-      throw err;
-    }
-  })
-});
-app.use(auth.filter);
+// // 错误处理
+// app.use((ctx, next) => {
+//   return next().catch((err) => {
+//     if (err.status === 401) {
+//       ctx.status = 401;
+//       ctx.body = 'Protected resource, use Authorization header to get access\n';
+//     } else {
+//       throw err;
+//     }
+//   })
+// });
+// app.use(auth.filter);
 app.use(logger());
 app.use(bodyParser());
 
