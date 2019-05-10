1fdf8ea9af32b4880319859a36887d6bd10c8b28
tzz <1789803837@qq.com>
2019-05-10 20:20:59 +0800

    完成cookie登录

--- routers/messageBoard.js
+++ routers/messageBoard.js
@@ -5,7 +5,9 @@ const smb = require('./../services/messageBoard');
 router.post('/index', smb.index);
 router.get('/index', async (ctx) => {
     console.log('index');
-    
+    if (!ctx.session.user) {
+        ctx.response.redirect('/login');
+    }
     await ctx.render('index', {});
 });
 
